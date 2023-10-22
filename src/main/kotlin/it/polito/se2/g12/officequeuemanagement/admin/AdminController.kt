1b94a2204e6c7783bee186c18c9f2dc3c83ab7a6
s292567 <101062653+s292567@users.noreply.github.com>
2023-10-22 16:26:28 +0200

    Create AdminController.kt

---
+++ src/main/kotlin/it/polito/se2/g12/officequeuemanagement/admin/AdminController.kt
@@ -0,0 +1,15 @@
+package it.polito.se2.g12.officequeuemanagement.admin
+
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@CrossOrigin
+class AdminController {
+    // Counter to do list
+    // TODO: make entry point get all counter,get single counter service list,change counter service list
+    // TODO: make entry point post add new counter
+    // Service to do list (globally usable services)
+    // TODO: make entry point get all service
+    // TODO: make entry point post add new service, change duration of service , delete a service
+}
