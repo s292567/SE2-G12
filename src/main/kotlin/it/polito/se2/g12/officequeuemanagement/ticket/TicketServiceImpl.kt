bf8c2a03cf81fed6f7efca50c02b2ab3143566bc
hermandall <gdalloni@gmail.com>
2023-10-24 21:41:11 +0200

    worked on createTicket

--- src/main/kotlin/it/polito/se2/g12/officequeuemanagement/ticket/TicketServiceImpl.kt
+++ src/main/kotlin/it/polito/se2/g12/officequeuemanagement/ticket/TicketServiceImpl.kt
@@ -5,21 +5,86 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TicketServiceImpl(private val ticketRepository: TicketRepository,private val serviceRepository: ServiceRepository):TicketService {
-    override fun createTicket(serviceName: String): TicketDTO {
+class TicketServiceImpl(
+        private val ticketRepository: TicketRepository,
+        private val serviceRepository: ServiceRepository,
+        private val counterRepository: CounterRepository
+) : TicketService {
 
-        TODO("Not yet implemented")
+    override fun createTicket(serviceName: String): TicketDTO {
+        // Retrieve the service by name from the serviceRepository
+        val services = serviceRepository.findByServiceName(serviceName)
+        if (services.isNotEmpty()) {
+            val service = services.first()
+            // Create a new Ticket
+            val ticket = Ticket(service = service)
+            // Save the ticket to the repository
+            val savedTicket = ticketRepository.save(ticket)
+            return TicketDTO(
+                    ticketId = savedTicket.TicketId,
+                    serviceType = serviceName,
+                    served = savedTicket.served,
+                    counterAssigned = savedTicket.counterAssigned
+            )
+
+        } else {
+            throw ServiceNotFoundException("Service not found for name: $serviceName")
+        }
     }
 
     override fun getTicketInfo(ticketId: Long): TicketDTO {
-        TODO("Not yet implemented")
+        // Retrieve the ticket by ID from the repository
+        val ticket = ticketRepository.findById(ticketId)
+                .orElseThrow { TicketNotFoundException("Ticket not found for ID: $ticketId") }
+
+        return TicketDTO(
+                ticketId = ticket.TicketId,
+                serviceType = ticket.service.tagName,
+                served = ticket.served,
+                counterAssigned = ticket.counterAssigned
+        )
     }
 
     override fun assignCounter(ticketId: Long, counterId: UUID): TicketDTO {
-        TODO("Not yet implemented")
+        // Retrieve the ticket by ID from the repository
+        val ticket = ticketRepository.findById(ticketId)
+                .orElseThrow { TicketNotFoundException("Ticket not found for ID: $ticketId") }
+
+        // Retrieve the counter by ID from the repository
+        val counter = counterRepository.findById(counterId)
+                .orElseThrow { CounterNotFoundException("Counter not found for ID: $counterId") }
+
+        // Assign the counter to the ticket
+        ticket.counterAssigned = counter
+
+        // Save the updated ticket
+        val updatedTicket = ticketRepository.save(ticket)
+
+        return TicketDTO(
+                ticketId = updatedTicket.TicketId,
+                serviceType = updatedTicket.service.tagName,
+                served = updatedTicket.served,
+                counterAssigned = updatedTicket.counterAssigned
+        )
     }
 
     override fun setServed(ticketId: Long): TicketDTO {
-        TODO("Not yet implemented")
+        // Retrieve the ticket by ID from the repository
+        val ticket = ticketRepository.findById(ticketId)
+                .orElseThrow { TicketNotFoundException("Ticket not found for ID: $ticketId") }
+
+        // Mark the ticket as served
+        ticket.served = true
+
+        // Save the updated ticket
+        val updatedTicket = ticketRepository.save(ticket)
+
+        return TicketDTO(
+                ticketId = updatedTicket.TicketId,
+                serviceType = updatedTicket.service.tagName,
+                served = updatedTicket.served,
+                counterAssigned = updatedTicket.counterAssigned
+        )
     }
 }
+
